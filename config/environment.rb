081237d27223a6f1885e37371263b7f8dc7bb964
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-23 19:40:10 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -13,3 +13,17 @@ SQL
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
+
+
+DB = {:conn => SQLite3::Database.new("db/artist.db")}
+DB[:conn].execute("DROP TABLE IF EXISTS artists")
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  net_worth INTEGER
+  )
+SQL
+
+DB[:conn].execute(sql)
+DB[:conn].results_as_hash = true
